43aa72504154a801f4d53426c11e59c862832818
DeenaPotnuru <72028098+DeenaPotnuru@users.noreply.github.com>
2020-09-28 20:21:21 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,10 @@
-echo "number of files in directory"
+files=3;
+echo "number of files in directory?";
+read data;
+if [ data > files]; then 
+echo "Files are high";
+elif [ data < files]; then
+echo "Files are low";
+else
+echo "Correct";
+fi
