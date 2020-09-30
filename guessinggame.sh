f43c179150094cc95c68cec657966e778edb705f
DeenaPotnuru <72028098+DeenaPotnuru@users.noreply.github.com>
2020-09-30 10:35:26 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,26 @@
-files=3;
-echo "number of files in directory?";
+files= $ls[wc-l]; 
+#user input
+
+echo "how many number of files in directory ?";
+echo "guess the number?";
+
+#read input
 read data;
-if [ data > files]; then 
+
+#check files
+function check{
+
+while [[ $data -gt 0]]
+do 
+
+if [ $data -gt $files]; then 
 echo "Files are high";
-elif [ data < files]; then
+
+elif [ $data -lt $files]; then
 echo "Files are low";
+
 else
 echo "Correct";
 fi
+done
+}
